f243a25b0ead0d076c29dc9c76e0a8c8d975249a
Junhyuk Yu <ericyoo0107@gmail.com>
2022-10-24 00:31:41 +0900

    1차 완성 (디폴트 수정)

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Subway subway = Subway.getInstance();
+		SubwayReport report = new SubwayReport();
+		subway.makeDefault();
+
+		while(true)
+		{
+			report.reportMain();
+		}
+	}
+}
