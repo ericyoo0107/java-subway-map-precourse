e775dbf6e7a4766e7951616ada71ea5a8ca1a850
Junhyuk Yu <ericyoo0107@gmail.com>
2022-10-24 18:03:27 +0900

    main이름 변경

--- src/Application.java
+++ src/Application.java
@@ -1,6 +1,6 @@
 import java.io.IOException;
 
-public class Main {
+public class Application {
 	public static void main(String[] args) throws IOException {
 		Subway subway = Subway.getInstance();
 		SubwayReport report = new SubwayReport();
